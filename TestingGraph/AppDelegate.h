f16f2aeedf2642b620b34443dd3fefced0477c63
nibab <nibab@cezars-macbook-pro.local>
2016-04-18 13:15:26 -0400

    Initial Commit

---
+++ TestingGraph/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingGraph
+//
+//  Created by Cezar Babin on 4/18/16.
+//  Copyright © 2016 senio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingGraph/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingGraph
+//
+//  Created by Cezar Babin on 4/18/16.
+//  Copyright © 2016 senio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
